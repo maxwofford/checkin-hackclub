d0c1c9b44166a83b57c4528c4a8657b915dcd371
Max Wofford <max@maxwofford.com>
2019-02-14 11:36:12 -0800

    Add password

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const triggerCheckIn = require('./airtable')
+const secrets = require('./secrets')
+
 app.get('/', (req, res) => {
-  require('./airtable.js')()
-  res.send('Hello World!')
+  const password = req.param('password')
+  if (password == secrets['PASSWORD']) {
+    triggerCheckIn()
+    res.send('Success!')
+  } else {
+    res.send("<a href='https://www.youtube.com/watch?v=RfiQYRn7fBg'>Ah ah ah, you didn't say the magic word</a>")
+  }
 })
 
 app.listen(port, () => console.log(`Started up and listening on port ${port}!`))
